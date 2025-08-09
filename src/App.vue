1536f39075f46977e519f5e00c328857396fc5a3
HigherGround189 <thehigherground18@gmail.com>
2025-08-10 03:49:36 +0800

    npm run format

--- src/App.vue
+++ src/App.vue
@@ -2,58 +2,63 @@
   <h1>Draggable Cards</h1>
 
   <div class="card-container">
-    <Cards v-for="(colour, index) in cardColourList" :key="index" :num="index" :colour="colour" v-model:zIndex="zIndexList[index]" :zIndexList="zIndexList"/>
+    <Cards
+      v-for="(colour, index) in cardColourList"
+      :key="index"
+      :num="index"
+      :colour="colour"
+      v-model:zIndex="zIndexList[index]"
+      :zIndexList="zIndexList"
+    />
   </div>
 </template>
 
 <script>
-  import Cards from './components/Cards.vue';
+import Cards from './components/Cards.vue'
 
-  function getRandomIntInclusive(min, max) {
-    const minCeiled = Math.ceil(min)
-    const maxFloored = Math.floor(max)
-    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled)
-  }
-  
-  const NUM_OF_CARDS = 9
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min)
+  const maxFloored = Math.floor(max)
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled)
+}
 
-  export default {
-    components: { Cards },
-    data() {
-      return {
-        cardColourList: [],
-        zIndexList: []
-      }
-    },
-    methods: {
+const NUM_OF_CARDS = 9
 
-    },
-    created() {
-      let randNum;
-      const colourList = ["red", "yellow", "green", "blue"]
-      for (let i=0; i <= NUM_OF_CARDS; i++) {
-        randNum = getRandomIntInclusive(0, 3)
-        this.cardColourList.push(colourList[randNum])
-        this.zIndexList.push(0)
-      }
+export default {
+  components: { Cards },
+  data() {
+    return {
+      cardColourList: [],
+      zIndexList: [],
     }
-  }
+  },
+  methods: {},
+  created() {
+    let randNum
+    const colourList = ['red', 'yellow', 'green', 'blue']
+    for (let i = 0; i <= NUM_OF_CARDS; i++) {
+      randNum = getRandomIntInclusive(0, 3)
+      this.cardColourList.push(colourList[randNum])
+      this.zIndexList.push(0)
+    }
+  },
+}
 </script>
 
 <style scoped>
-  h1 {
-    color: white;
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 5.5rem;
-    text-align: center;
-    margin-bottom: 10vh;
-    text-shadow: 0 0 5px #aaaaaa;
-  }
+h1 {
+  color: white;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 5.5rem;
+  text-align: center;
+  margin-bottom: 10vh;
+  text-shadow: 0 0 5px #aaaaaa;
+}
 
-  .card-container {
-    display: flex;
-    gap: 5vw; 
-    flex-wrap: wrap;
-    justify-content: center;
-  }
+.card-container {
+  display: flex;
+  gap: 5vw;
+  flex-wrap: wrap;
+  justify-content: center;
+}
 </style>
--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="card" ref="card" :style="{ zIndex: zIndex }" v-on="{ click: zIndexIncrement, mousedown: mouseDown, mouseup: mouseUp }">
+  <div
+    class="card"
+    ref="card"
+    :style="{ zIndex: zIndex }"
+    v-on="{ click: zIndexIncrement, mousedown: mouseDown, mouseup: mouseUp }"
+  >
     <div class="card-inner" :style="{ background: `var(--${colour})` }">
       <div class="card-number">
         <span :class="underline">{{ num }}</span>
@@ -9,124 +14,119 @@
 </template>
 
 <script>
-  import interact from 'interactjs';
+import interact from 'interactjs'
 
-  export default {
-    props: ["num", "colour", "zIndex", "zIndexList"],
-    data() {
-      return {
-
+export default {
+  props: ['num', 'colour', 'zIndex', 'zIndexList'],
+  data() {
+    return {}
+  },
+  computed: {
+    underline() {
+      let underlineClass = ''
+      if (this.$props.num === 6 || this.$props.num === 9) {
+        underlineClass = 'underline'
       }
+
+      return underlineClass
     },
-    computed: {
-      underline() {
-        let underlineClass = ""
-        if (this.$props.num === 6 || this.$props.num === 9) {
-          underlineClass = "underline"
-        }
+  },
+  methods: {
+    makeDraggable() {
+      const position = { x: 0, y: 0 }
 
-        return underlineClass
-      }
+      interact(this.$refs.card).draggable({
+        modifiers: [
+          interact.modifiers.restrictRect({
+            restriction: 'body',
+            endOnly: true,
+          }),
+        ],
+        cursorChecker: () => null,
+        listeners: {
+          move(event) {
+            position.x += event.dx
+            position.y += event.dy
+
+            event.target.style.transform = `translate(${position.x}px, ${position.y}px)`
+          },
+        },
+      })
     },
-    methods: {
-      makeDraggable() {
-        const position = { x: 0, y: 0 };
-
-        interact(this.$refs.card).draggable({
-          modifiers: [
-            interact.modifiers.restrictRect({
-              restriction: "body",
-              endOnly: true
-            })
-          ],
-          cursorChecker: () => null,
-          listeners: {
-            move (event) {
-              position.x += event.dx
-              position.y += event.dy
-
-              event.target.style.transform =
-                `translate(${position.x}px, ${position.y}px)`
-            },
-          }
-        })
-      },
-      zIndexIncrement() {
-        const zIndexListMax = Math.max(...this.$props.zIndexList)
-        this.$emit('update:zIndex', zIndexListMax + 1)
-      },
-      mouseDown() {
-        this.$refs.card.style.zIndex = 9999999999999
-        document.style
-      },
-      mouseUp() {
-        this.$refs.card.style.zIndex = this.zIndex
-      }
+    zIndexIncrement() {
+      const zIndexListMax = Math.max(...this.$props.zIndexList)
+      this.$emit('update:zIndex', zIndexListMax + 1)
     },
-    mounted() {
-      this.makeDraggable()
-    }
-  }
-
+    mouseDown() {
+      this.$refs.card.style.zIndex = 9999999999999
+      document.style
+    },
+    mouseUp() {
+      this.$refs.card.style.zIndex = this.zIndex
+    },
+  },
+  mounted() {
+    this.makeDraggable()
+  },
+}
 </script>
 
 <style scoped>
-  .card {
-    background: white;
-    padding: 5px;
-    /* opacity: 0.75; */
+.card {
+  background: white;
+  padding: 5px;
+  /* opacity: 0.75; */
 
-    width: 116px;
-    height: 178px;
-    border-radius: 5px;
-    box-shadow:0 0 10px #aaaaaa;
+  width: 116px;
+  height: 178px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px #aaaaaa;
 
-    cursor: grab;
-    user-select: none;
-    touch-action: none;
-    transition: 0.3s transform;
-  }
+  cursor: grab;
+  user-select: none;
+  touch-action: none;
+  transition: 0.3s transform;
+}
 
-  .card:hover {
-    opacity: 1;
-  }
+.card:hover {
+  opacity: 1;
+}
 
-  .card:active {
-    cursor: grabbing;
-    transition: none;
-  }
+.card:active {
+  cursor: grabbing;
+  transition: none;
+}
 
-  .card-inner {
-    --red: hsl(0, 80%, 40%);
-    --green: hsl(150, 50%, 50%);
-    --blue: hsl(235, 50%, 40%);
-    --yellow: hsl(50, 75%, 50%);
+.card-inner {
+  --red: hsl(0, 80%, 40%);
+  --green: hsl(150, 50%, 50%);
+  --blue: hsl(235, 50%, 40%);
+  --yellow: hsl(50, 75%, 50%);
 
-    background: var(--blue);
-    border-radius: 5px;
-    height: 100%;
-    width: 100%;
-  }
+  background: var(--blue);
+  border-radius: 5px;
+  height: 100%;
+  width: 100%;
+}
 
-  .card-number {
-    color: white;
+.card-number {
+  color: white;
 
-    font-family: "Poppins", sans-serif;
-    font-weight: bolder;
-    font-size: 66px;
+  font-family: 'Poppins', sans-serif;
+  font-weight: bolder;
+  font-size: 66px;
 
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-  .underline {
-    text-decoration: underline;
-    text-underline-offset: 0.5rem;
-    text-decoration-thickness: 0.4rem;
-  }
-
+.underline {
+  text-decoration: underline;
+  text-underline-offset: 0.5rem;
+  text-decoration-thickness: 0.4rem;
+}
 </style>
